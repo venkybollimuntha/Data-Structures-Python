5410c1cb968673b11482a5cc794f7a0433e8ba6a
Venky Bollimuntha <venkybollimuntha@gmail.com>
2020-09-14 00:23:54 +0530

    Create balance_brackets_optimized.py

---
+++ Stack/balance_brackets_optimized.py
@@ -0,0 +1,19 @@
+def is_match(ch1, ch2):
+    match_dict = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match_dict[ch1] == ch2
+def is_balanced(s):
+    stack = Stack()
+    for ch in s:
+        if ch=='(' or ch=='{' or ch == '[':
+            stack.push(ch)
+        if ch==')' or ch=='}' or ch == ']':
+            if stack.size()==0:
+                return False
+            if not is_match(ch,stack.pop()):
+                return False
+
+    return stack.size()==0
